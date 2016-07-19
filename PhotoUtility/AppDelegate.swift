533b13c7d76b66911742590d3adca4118f23c911
Jason Sturges <jason@jasonsturges.com>
2016-07-19 13:23:21 -0500

    Initial Commit

---
+++ PhotoUtility/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhotoUtility
+//
+//  Created by Jason Sturges on 7/19/16.
+//  Copyright © 2016 Jason Sturges. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PhotoUtility/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PhotoUtility
+//
+//  Created by Jason Sturges on 7/19/16.
+//  Copyright © 2016 Jason Sturges. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
