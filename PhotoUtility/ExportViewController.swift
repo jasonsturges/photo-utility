55e7bc9c7a16d18872fc90c251df863b0281507f
Jason Sturges <jason@jasonsturges.com>
2016-07-19 13:54:40 -0500

    Tab view controller storyboards

---
+++ PhotoUtility/ExportViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExportViewController.swift
+//  PhotoUtility
+//
+//  Created by Jason Sturges on 7/19/16.
+//  Copyright © 2016 Jason Sturges. All rights reserved.
+//
+
+import Cocoa
+
+class ExportViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ PhotoUtility/ImportViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ImportViewController.swift
+//  PhotoUtility
+//
+//  Created by Jason Sturges on 7/19/16.
+//  Copyright © 2016 Jason Sturges. All rights reserved.
+//
+
+import Cocoa
+
+class ImportViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
